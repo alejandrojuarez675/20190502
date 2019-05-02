af9cfff7c7e8eb5d784fdd39a18d1cd9edff7628
aleja <aleja>
2019-05-02 20:01:51 -0300

    moidifico script

--- Assets/Scripts/Download.cs
+++ Assets/Scripts/Download.cs
@@ -3,17 +3,24 @@ using UnityEngine;
 using UnityEngine.Networking;
 public class Download : MonoBehaviour
 {
+
+	public string url;
+	public string nombreObjecto;
+	public int positionX=0;
+	public int positionY=0;
+	public int positionZ=0;
+
+
     private GameObject _myPrefab;
     IEnumerator Start()
     {
-        string url = "https://s3-us-west-2.amazonaws.com/aquabuckettest/auto.1";
         using (var uwr = new UnityWebRequest(url, UnityWebRequest.kHttpVerbGET))
         {
             uwr.downloadHandler = new DownloadHandlerAssetBundle(url, 0);
             yield return uwr.Send();
             AssetBundle bundle = DownloadHandlerAssetBundle.GetContent(uwr);
-            _myPrefab = bundle.LoadAsset("Car") as GameObject;
-            _myPrefab.transform.position = new Vector3(10, 1, 10);
+            _myPrefab = bundle.LoadAsset(nombreObjecto) as GameObject;
+            _myPrefab.transform.position = new Vector3(positionX, positionY, positionZ);
 			Instantiate(_myPrefab);
         }
     }
